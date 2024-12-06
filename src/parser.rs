8692d32cef99116e331ac2596054b05b3bc6932f
Wojciech Jedynak <wjedynak@gmail.com>
2024-12-06 09:01:07 +0900

    parse color

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,13 @@
 use crate::game::*;
 
+fn parse_color(color: &str) -> Option<Color> {
+    match color {
+        "b" => Some(Color::Black),
+        "w" => Some(Color::White),
+        _ => None,
+    }
+}
+
 fn parse_piece(piece: &str) -> Option<Piece> {
     match piece {
         "p" => Some(WHITE_PAWN),
@@ -21,6 +29,21 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_parse_color() {
+        let inputs = [
+            ("b", Some(Color::Black)),
+            ("w", Some(Color::White)),
+            ("", None),
+            ("!", None),
+        ];
+
+        for (string, expected_result) in inputs {
+            let result = parse_color(string);
+            assert_eq!(result, expected_result);
+        }
+    }
+
+    #[test]
     fn test_parse_piece_correct() {
         let inputs = [
             ("p", WHITE_PAWN),
@@ -37,7 +60,19 @@ mod tests {
 
         for (string, expected_piece) in inputs {
             let result = parse_piece(string);
+            assert_eq!(result, Some(expected_piece));
+        }
+    }
+
+    #[test]
+    fn test_parse_piece_incorrect() {
         let inputs = [
+            "", "x", "123", "n", "g", "s", // shogi pieces not in Let's catch the Lion
+        ];
+
+        for string in inputs {
+            let result = parse_piece(string);
+            assert_eq!(result, None);
         }
     }
 }
